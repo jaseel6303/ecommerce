e9c9c09a172155291808c84dfeee4dcb9cad65c0
jaseel6303 <jaseeljasee1997@gmail.com>
2023-05-11 16:28:33 +0530

    product model created

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY_CHOICES=(
+    ('MC','Mobile Case'),
+    ('SP','Screen Protector'),
+    ('AC','Accessories'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=50)
+    product_image = models.ImageField(upload_to='products')
+    def __str__(self):
+        return self.title

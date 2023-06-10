637ad5a73c8ec1f6b284cd88d10e8f322088a9f7
jaseel6303 <jaseeljasee1997@gmail.com>
2023-06-10 11:50:40 +0530

    address,address update

--- ecommerce/app/views.py
+++ ecommerce/app/views.py
@@ -1,6 +1,6 @@
 from itertools import count
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from . models import Product,Customer
 from . forms import CustomerRegistrationForm,CustomerProfileForm
@@ -81,8 +81,21 @@ def address(request):
 
 class updateAddress(View):
     def get(self,request,pk):
-        form = CustomerProfileForm()
+        addr = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=addr)
         return render(request,'app/updateAddress.html',locals())
     def post(self,request,pk):
         form = CustomerProfileForm(request.POST)
-        return render(request, 'app/updateAddress.html',locals())
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.pincode = form.cleaned_data['pincode']
+            add.save()
+            messages.success(request,"Congratulations! Profile Update Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return redirect("address")
